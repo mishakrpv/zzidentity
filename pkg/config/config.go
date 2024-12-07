4d3a0f7439919ec148cec8bee06756c088282357
mishakrpv <mishavkrpv@gmail.com>
2024-12-07 18:51:26 +0300

    style: add spaces in switch case

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -138,34 +138,41 @@ func populate(cfg interface{}, withFn func(key string) string, prefix string) er
 		}
 
 		switch field.Kind() {
+			
 		case reflect.String:
 			field.SetString(envValue)
+			
 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 			intValue, err := strconv.ParseInt(envValue, 10, 64)
 			if err != nil {
 				return err
 			}
 			field.SetInt(intValue)
+
 		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 			uintValue, err := strconv.ParseUint(envValue, 10, 64)
 			if err != nil {
 				return err
 			}
 			field.SetUint(uintValue)
+
 		case reflect.Float32, reflect.Float64:
 			floatValue, err := strconv.ParseFloat(envValue, 64)
 			if err != nil {
 				return err
 			}
 			field.SetFloat(floatValue)
+
 		case reflect.Bool:
 			boolValue, err := strconv.ParseBool(envValue)
 			if err != nil {
 				return err
 			}
 			field.SetBool(boolValue)
+
 		default:
 			return fmt.Errorf("unsupported type for field '%s': %s", tag, field.Kind())
+
 		}
 	}
 
